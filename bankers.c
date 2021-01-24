0d1e204054b0fa9fc18d7ff39420f5ba17933475
liyaelizebeth <elizebethliya1@gmail.com>
2021-01-24 18:45:27 +0530

    added bankers

--- bankers.c
+++ bankers.c
@@ -1,49 +1,114 @@
-#include <stdio.h>
-
-void main()
+#include<stdio.h>
+int main()
 {
-    int m, n, i, j;
-    int alloc[10][10], maxi[10][10], need[10][10];
-    int avail[10], seq[10];
+   int n,m,i,j,k;
+   int count =0;
+   
+   int f[10];
+   int alloc[100][100],max[100][100],need[100][100];
+   int avail[10],seq[10];
+   printf("\nENTER NO OF PROCESSES");
+   scanf("%d",&n);
+   printf("\nENTER NO OF RESOURCES");
+   scanf("%d",&m);
+   for(i=0;i<n;i++)
+   {  
+      printf("\nENTER ALLOCATION MATRIX OF P%d",i);
+      for(j=0;j<m;j++)
+      {
+         scanf("%d",&alloc[i][j]);
+         f[i]=0;
 
-    printf("\nenter no of processes");
-    scanf("%d", &m);
-    printf("enter no of resources");
-    scanf("%d", &n);
-    for (i = 0; i < m; i++)
-    {
-        printf("\nenter allocation matrix of P%d", i);
-        for (j = 0; j < n; j++)
-        {
-            scanf("%d", &alloc[i][j]);
-        }
-    }
+      }
+   }
+   for(i=0;i<n;i++)
+   {  
+      printf("\nENTER MAXIMUM MATRIX OF P %d",i);
+      for(j=0;j<m;j++)
+      {
+         scanf("%d",&max[i][j]);
+      }
+   }
+   
+   for(j=0;j<m;j++)
+   {
+      printf("\nENTER NO OF AVAILABLE INSTANCES OF RESOURCE%d",j);
+      scanf("%d",&avail[j]);
+   }
+   for(i=0;i<n;i++)
+   {  
+      
+      for(j=0;j<m;j++)
+      {
+         need[i][j] = max[i][j] - alloc[i][j];
 
-    for (i = 0; i < m; i++)
-    {
-        printf("\nenter maximum matrix of P%d", i);
-        for (j = 0; j < n; j++)
-        {
-            scanf("%d", &maxi[i][j]);
-        }
-    }
+      }
+   }
+   for(i=0;i<n;i++)
+   {  
+       printf("\n NEED MATRIX OF P%d",i);
 
-    for (i = 0; i < m; i++)
-    {
-
-        for (j = 0; j < n; j++)
-        {
-            need[i][j] = maxi[i][j] - alloc[i][j];
-        }
-    }
-
-    for (i = 0; i < m; i++)
-    {
-        printf("\nNeed matrix of P%d", i);
-
-        for (j = 0; j < m; j++)
-        {
-            printf("\t%d", need[i][j]);
-        }
-    }
+      for(j=0;j<m;j++)
+      {
+         printf("\t %d",need[i][j]);
+        
+      }
+   }
+   for (k=0;k<n;k++){
+   for(i=0;i<n;i++)
+   {  
+       
+      if ( f[i] == 0)
+      {
+           for(j=0;j<m;j++)
+           {
+               if(need[i][j] <= avail[j])
+               {
+                    continue;
+               }
+               else
+               {
+                     break;
+               }
+            }
+            if(j==m)
+            {
+                 f[i] = 1;
+                 for(j=0;j<m;j++)
+                 {
+                      avail[j] = avail[j]+alloc[i][j];
+                  }
+                  count++;
+                  seq[count - 1]=i;
+             }
+         }
+      }
 }
+      if (count == n)
+      {
+         printf("\nSAFE SEQUENCE");
+         for(i=0;i<n;i++)
+         {
+            printf("\tP%d",seq[i]);
+         }
+       }
+       else
+          printf("\n NO");
+
+
+                 
+
+      
+   
+
+
+      
+     
+   
+}
+   
+          
+
+   
+
+
